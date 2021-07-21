51c57093ecebf9ca1c1c0710102db2084d63b6eb
prathmeshTamhan <prathmeshtamhan28@gmail.com>
2021-07-21 11:27:04 +0530

    Carpooling app

---
+++ root/rider-frontend/src/components/PrivateRoutes/PrivateRoute.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import configRoute from '../../configs/roles';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import '../../styles/PrivateRoute.css';
+
+function PrivateRoute(props) {
+  return (
+    <div className='main-container'>
+      <Switch>
+        {configRoute[props.role].route.map(route => {
+          console.log('private role', props.role);
+          return (
+            <Route
+              exact
+              path={route.url}
+              key={route.url}
+              render={routeProps => (
+                <route.component
+                  {...routeProps}
+                  role={props.role}
+                  setRole={props.setRole}
+                  userInfo={props.userInfo}
+                  socketRef={props.socketRef}
+                />
+              )}
+            />
+          );
+        })}
+
+        <Redirect to={configRoute[props.role].redirect} />
+      </Switch>
+    </div>
+  );
+}
+
+export default PrivateRoute;
